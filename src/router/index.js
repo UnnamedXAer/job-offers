e5b81e4fe1371019012cdc867158da2c4abe7122
UnnamedXAer <UnnamedXAer@gmail.com>
2021-07-05 15:14:51 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home';
+import HomeVue from '@/components/Home';
+import LookupVue from '@/components/lookup/Lookup.vue';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: HomeVue
+    },
+    {
+      path: '/search',
+      name: 'Lookup',
+      component: LookupVue
     }
   ]
 });
