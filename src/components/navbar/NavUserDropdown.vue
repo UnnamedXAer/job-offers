e614d2233c5868529c21ecfe196e3084b9a57766
UnnamedXAer <UnnamedXAer@gmail.com>
2021-07-07 15:18:43 +0200

    enable dropdown / add animations

---
+++ src/components/navbar/NavUserDropdown.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="nav-item dropdown">
+    <a
+      class="nav-link fs-3 fw-bold dropdown-toggle"
+      href="#"
+      id="navbarUserDropdownMenuLink"
+      role="button"
+      data-bs-toggle="dropdown"
+      :aria-expanded="expanded"
+      @click.stop="toggleUserMenu"
+    >
+      User
+    </a>
+    <transition name="__fade-scale">
+      <div
+        class="dropdown-menu show"
+        v-if="expanded"
+        aria-labelledby="navbarUserDropdownMenuLink"
+      >
+        <div class="d-flex">
+          <router-link
+            class="dropdown-item"
+            to="/user/account"
+            @click.native.stop="toggleUserMenu"
+            ><i class="bi bi-person fs-5 pe-3"></i>
+            <span class="nav-text">Dean Winchester</span></router-link
+          >
+        </div>
+        <hr class="dropdown-divider" />
+        <router-link
+          class="dropdown-item"
+          to="/offers/new"
+          @click.native.stop="toggleUserMenu"
+          >Create New Offer</router-link
+        >
+        <hr class="dropdown-divider" />
+        <button class="dropdown-item" @click.stop="toggleUserMenu">
+          Logout
+        </button>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      expanded: false
+    };
+  },
+  watch: {
+    expanded(val) {
+      if (val) {
+        return document.addEventListener('click', this.closeMenu);
+      }
+      return document.removeEventListener('click', this.closeMenu);
+    }
+  },
+  methods: {
+    toggleUserMenu() {
+      console.log('user --- toogle to: ', !this.expanded);
+      this.expanded = !this.expanded;
+    },
+    closeMenu(ev) {
+      console.log('user --- close user menu', ev.target);
+      if (!this.$el.contains(ev.target)) {
+        this.expanded = false;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.__fade-scale-enter,
+.__fade-scale-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.__fade-scale-enter-active,
+.__fade-scale-leave {
+  transition: opacity 300ms, transform 300ms;
+}
+
+.__fade-scale-leave-active {
+  transition: opacity 300ms, transform 300ms;
+  transform: scale(0.8);
+  opacity: 0;
+}
+</style>
--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,53 +1,104 @@
 <template>
   <header>
     <nav
-      class="
-        navbar navbar-expand-md navbar-light
-        shadow-sm
-        p-3
-        mb-5
-        __navbar
-        rounded-bottom
-      "
+      class="navbar navbar-expand-md navbar-light shadow-sm p-3 mb-5 __navbar"
     >
       <div class="container-lg">
         <router-link class="navbar-brand fs-2" to="/">💸</router-link>
         <button
-          class="navbar-toggler"
+          class="navbar-toggler collapsed"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNavDropdown"
           aria-controls="navbarNavDropdown"
-          aria-expanded="false"
           aria-label="Toggle navigation"
+          :aria-expanded="expanded"
+          @click.stop="toggleMenu"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <div class="navbar-nav">
-            <router-link
-              to="/"
-              active-class="active"
-              class="nav-link fs-3 fw-bold"
-              exact
-            >
-              Home
-            </router-link>
-            <router-link
-              to="/lookup"
-              active-class="active"
-              class="nav-link fs-3 fw-bold"
-            >
-              Lookup
-            </router-link>
+        <transition name="__fade-scale">
+          <div class="navbar-collapse" id="navbarNavDropdown" v-show="expanded">
+            <div class="navbar-nav">
+              <router-link
+                to="/"
+                active-class="active"
+                class="nav-link fs-3 fw-bold"
+                exact
+                @click.native.stop="toggleMenu"
+              >
+                Home
+              </router-link>
+              <router-link
+                to="/lookup"
+                active-class="active"
+                class="nav-link fs-3 fw-bold"
+                @click.native.stop="toggleMenu"
+              >
+                Lookup
+              </router-link>
+              <app-nav-user-dropdown></app-nav-user-dropdown>
+            </div>
           </div>
-        </div>
+        </transition>
       </div>
     </nav>
   </header>
 </template>
+
+<script>
+import NavUserDropdownVue from './NavUserDropdown.vue';
+export default {
+  components: {
+    appNavUserDropdown: NavUserDropdownVue
+  },
+  data() {
+    return {
+      expanded: false
+    };
+  },
+  watch: {
+    expanded(val) {
+      if (val) {
+        return document.addEventListener('click', this.closeMenu);
+      }
+      return document.removeEventListener('click', this.closeMenu);
+    }
+  },
+  methods: {
+    toggleMenu() {
+      console.log('toogle to: ', !this.expanded);
+      this.expanded = !this.expanded;
+    },
+    closeMenu(ev) {
+      console.log('close menu', ev.target);
+      if (!this.$el.contains(ev.target)) {
+        this.expanded = false;
+      }
+    }
+  }
+};
+</script>
+
 <style scoped>
 .__navbar {
   background-color: #c0e2fa;
+}
+
+.__fade-scale-enter,
+.__fade-scale-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.__fade-scale-enter-active,
+.__fade-scale-leave {
+  transition: opacity 300ms, transform 300ms;
+}
+
+.__fade-scale-leave-active {
+  transition: opacity 300ms, transform 300ms;
+  transform: scale(0.8);
+  opacity: 0;
 }
 </style>
