8e231329a075efa5bd008dce1bafc5699a4230fe
UnnamedXAer <unnamedxaer.git@gmail.com>
2021-07-08 21:53:52 +0200

    extract offer data to separate component

--- src/components/lookup/offers/Offer.vue
+++ src/components/lookup/offers/Offer.vue
@@ -1,72 +1,6 @@
 <template>
   <div class="d-flex flex-column mb-5 __offer">
-    <section class="p-lg-5 p-sm-3 p-1">
-      <h4 class="px-lg-4" role="h1" style="padding-top: 0 !important">
-        {{ offer.title }}
-      </h4>
-      <div class="__offer-details">
-        <div>
-          <small>{{ offer.company }}</small>
-        </div>
-        <div class="d-flex justify-content-between">
-          <div>
-            <div
-              class="badge bg-success m-1"
-              :class="{ 'bg-warning': typeof loc === 'string' }"
-              v-for="(loc, idx) in offer.locations"
-              :key="idx"
-            >
-              {{ typeof loc === 'string' ? loc : loc.name }}
-              <i class="bi bi-geo-alt" v-if="typeof loc === 'object'"></i>
-            </div>
-          </div>
-          <code v-if="offer.salary"
-            >$ {{ offer.salary.start * 1000 }} -
-            {{ offer.salary.end * 1000 }}</code
-          >
-        </div>
-      </div>
-      <article class="mb-3">
-        <p>{{ offer.description }}</p>
-      </article>
-      <article class="__stack mb-3" v-if="offer.stack">
-        <h5 role="h2">Tech Stack</h5>
-        <div class="d-flex flex-row justify-content-center flex-wrap">
-          <div
-            class="badge bg-primary m-1"
-            v-for="tech in offer.stack"
-            :key="tech.tech"
-          >
-            {{ tech.tech }}
-            <small style="color: #ccc; font-size: 0.8em">{{ tech.lv }}</small>
-          </div>
-        </div>
-      </article>
-      <article class="__requirements" v-if="offer.requirements">
-        <h5 role="h2">Requirements</h5>
-        <ul class="__reqs_list __list">
-          <li v-for="req in offer.requirements" :key="req">
-            <i class="bi bi-bookmark-check"></i> {{ req }}
-          </li>
-        </ul>
-      </article>
-      <article class="__tasks" v-if="offer.tasks">
-        <h5 role="h2">Tasks</h5>
-        <ul class="__list">
-          <li v-for="task in offer.tasks" :key="task">
-            <i class="bi bi-screwdriver"></i> {{ task }}
-          </li>
-        </ul>
-      </article>
-      <article class="__benefits" v-if="offer.benefits">
-        <h5 role="h2">Benefits</h5>
-        <ul class="__list">
-          <li v-for="benefit in offer.benefits" :key="benefit">
-            <i class="bi bi-gift" style="font-size: 0.8em"></i> {{ benefit }}
-          </li>
-        </ul>
-      </article>
-    </section>
+    <app-offer :offer="offer"></app-offer>
 
     <div class="position-sticky bottom-0 pb-2 __offer_actions">
       <app-rule class="mb-2"></app-rule>
@@ -133,9 +67,11 @@
 <script>
 import FadeRuleVue from '../../ui/FadeRule.vue';
 import firebaseAxios from '../../../axios/firebase';
+import OfferVue from '../../offers/Offer.vue';
 
 export default {
   components: {
+    appOffer: OfferVue,
     appRule: FadeRuleVue
   },
   props: {
@@ -236,32 +172,6 @@ export default {
 </script>
 
 <style scoped>
-.__offer-details {
-  min-height: 4rem;
-}
-
-.__list {
-  padding-left: 0.5rem;
-}
-.__list li {
-  list-style-type: none;
-}
-
-.__list li i {
-  margin-right: 1rem;
-}
-
-.__benefits .__list li i {
-  color: #57038f;
-}
-
-.__requirments .__list li i {
-  color: #0c5301;
-}
-.__tasks .__list li i {
-  color: #411801;
-}
-
 .__offer_actions {
   background-color: rgba(250, 250, 250, 0.7);
   background: rgb(207, 207, 207);
---
+++ src/components/offers/Offer.vue
@@ -0,0 +1,108 @@
+<template>
+  <section class="p-lg-5 p-sm-3 p-1">
+    <h4 class="px-lg-4" role="h1" style="padding-top: 0 !important">
+      {{ offer.title }}
+    </h4>
+    <div class="__offer-details">
+      <div>
+        <small>{{ offer.company }}</small>
+      </div>
+      <div class="d-flex justify-content-between">
+        <div>
+          <div
+            class="badge bg-success m-1"
+            :class="{ 'bg-warning': typeof loc === 'string' }"
+            v-for="(loc, idx) in offer.locations"
+            :key="idx"
+          >
+            {{ typeof loc === 'string' ? loc : loc.name }}
+            <i class="bi bi-geo-alt" v-if="typeof loc === 'object'"></i>
+          </div>
+        </div>
+        <code v-if="offer.salary"
+          >$ {{ offer.salary.start * 1000 }} -
+          {{ offer.salary.end * 1000 }}</code
+        >
+      </div>
+    </div>
+    <article class="mb-3">
+      <p>{{ offer.description }}</p>
+    </article>
+    <article class="__stack mb-3" v-if="offer.stack">
+      <h5 role="h2">Tech Stack</h5>
+      <div class="d-flex flex-row justify-content-center flex-wrap">
+        <div
+          class="badge bg-primary m-1"
+          v-for="tech in offer.stack"
+          :key="tech.tech"
+        >
+          {{ tech.tech }}
+          <small style="color: #ccc; font-size: 0.8em">{{ tech.lv }}</small>
+        </div>
+      </div>
+    </article>
+    <article class="__requirements" v-if="offer.requirements">
+      <h5 role="h2">Requirements</h5>
+      <ul class="__reqs_list __list">
+        <li v-for="req in offer.requirements" :key="req">
+          <i class="bi bi-bookmark-check"></i> {{ req }}
+        </li>
+      </ul>
+    </article>
+    <article class="__tasks" v-if="offer.tasks">
+      <h5 role="h2">Tasks</h5>
+      <ul class="__list">
+        <li v-for="task in offer.tasks" :key="task">
+          <i class="bi bi-screwdriver"></i> {{ task }}
+        </li>
+      </ul>
+    </article>
+    <article class="__benefits" v-if="offer.benefits">
+      <h5 role="h2">Benefits</h5>
+      <ul class="__list">
+        <li v-for="benefit in offer.benefits" :key="benefit">
+          <i class="bi bi-gift" style="font-size: 0.8em"></i> {{ benefit }}
+        </li>
+      </ul>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    offer: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.__offer-details {
+  min-height: 4rem;
+}
+
+.__list {
+  padding-left: 0.5rem;
+}
+.__list li {
+  list-style-type: none;
+}
+
+.__list li i {
+  margin-right: 1rem;
+}
+
+.__benefits .__list li i {
+  color: #57038f;
+}
+
+.__requirments .__list li i {
+  color: #0c5301;
+}
+.__tasks .__list li i {
+  color: #411801;
+}
+</style>
