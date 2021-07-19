85f4e46aa50706d7379bfae0d9cdb453c321c88f
UnnamedXAer <UnnamedXAer@gmail.com>
2021-07-19 13:06:04 +0200

    allow dotenv vars for development

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,31 @@
-"use strict";
-const merge = require("webpack-merge");
-const prodEnv = require("./prod.env");
+'use strict';
+const merge = require('webpack-merge');
+const prodEnv = require('./prod.env');
 
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
-});
+const { parsed: dotEnv } = require('dotenv').config();
+
+console.log('dotenv: ', dotEnv);
+
+if (!dotEnv) {
+  dotEnv = {};
+}
+
+for (const key in dotEnv) {
+  if (!dotEnv[key].startsWith('"')) {
+    dotEnv[key] = '"' + dotEnv[key] + '"';
+  }
+
+  if (!dotEnv[key].startsWith('"') || !dotEnv[key].endsWith('"')) {
+    console.error(
+      "error: env variable '" + key + "' have missing double quote"
+    );
+  }
+}
+
+module.exports = merge(
+  prodEnv,
+  {
+    NODE_ENV: '"development"'
+  },
+  dotEnv
+);
