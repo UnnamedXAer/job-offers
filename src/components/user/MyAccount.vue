632cd6d66c2a9e65e5373925974280e80074df61
UnnamedXAer <UnnamedXAer@gmail.com>
2021-07-29 15:11:14 +0200

    add content to profile component

--- src/components/user/MyAccount.vue
+++ src/components/user/MyAccount.vue
@@ -1,21 +1,150 @@
 <template>
   <div class="">
-    <h1>Hello {{ user.fname }}</h1>
+    <section class="row mb-4">
+      <div class="col-12 col-sm-5 __avatar_container">
+        <div class="mx-auto __avatar_wrapper">
+          <img
+            class="img-thumbnail __avatar"
+            :src="user.avatarUrl ? user.avatarUrl : '@/assets/icons/user.svg'"
+            alt="Avatar image"
+          />
+        </div>
+      </div>
+
+      <div class="col-12 col-sm-7">
+        <table>
+          <tr>
+            <th>Name</th>
+            <td>{{ user.fname }} {{ user.lname }}</td>
+          </tr>
+          <tr>
+            <th>DOB</th>
+            <td>{{ user.DOB }}</td>
+          </tr>
+          <tr>
+            <th>Title</th>
+            <td>{{ user.title }}</td>
+          </tr>
+        </table>
+      </div>
+    </section>
+
+    <section class="row row-cols-1 row-cols-md-2 g-4" v-if="userDetails">
+      <div class="col">
+        <div class="card card-body">
+          <h5 class="card-title">Education</h5>
+          <ul>
+            <li
+              class="mb-2"
+              v-for="(edu, idx) in userDetails.educations"
+              :key="idx"
+            >
+              <h6 class="card-text">
+                {{ edu.school }}
+              </h6>
+              <p style="margin-block-end: 0">
+                {{ edu.field }}
+              </p>
+              <small class="card-text text-secondary">
+                {{ edu.start | format }} - {{ edu.end | format }}
+              </small>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card card-body">
+          <h5 class="card-title">Knowledge</h5>
+          <ul>
+            <li
+              class="card-text"
+              v-for="(know, idx) in userDetails.knowledge"
+              :key="idx"
+            >
+              {{ know.name }} <small v-if="know.lv">{{ know.lv }}</small>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card card-body">
+          <h5 class="card-title">Expirience</h5>
+          <ul>
+            <li
+              class="card-text"
+              v-for="(exp, idx) in userDetails.experience"
+              :key="idx"
+            >
+              <h6>{{ exp.companyName }}</h6>
+              <p style="margin-block-end: 0">
+                {{ exp.position }}
+              </p>
+              <small class="card-text text-secondary">
+                {{ exp.start | format }} -
+                <span v-if="exp.end">{{ exp.end | format }}</span>
+                <span v-else>currently</span>
+              </small>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card card-body">
+          <h5 class="card-title">Hobbies</h5>
+          <ul>
+            <li
+              class="card-text"
+              v-for="(hobby, idx) in userDetails.hobbies"
+              :key="idx"
+            >
+              {{ hobby }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import format from 'date-fns/format';
 import { mapState } from 'vuex';
+
 export default {
+  filters: {
+    format: (val) => {
+      if (!val || val === 'current') {
+        return 'current';
+      }
+      return format(val, 'yyyy-MM-dd');
+    }
+  },
   name: 'MyProfile',
   computed: {
     ...mapState({
-      user: (state) => state.auth.user
+      user: (state) => state.auth.user,
+      userDetails: (state) => state.auth.userDetails
     })
   },
-  created: {}
+  created() {
+    if (!this.$store.state.auth.userDetails) {
+      this.$store.dispatch('fetchLoggedUserDetails');
+    }
+  }
 };
 </script>
 
 <style scoped>
+.__avatar_wrapper,
+.__avatar {
+  width: 200px;
+  height: 200px;
+}
+
+.__avatar {
+  border-radius: 50%;
+}
 </style>
--- src/store/auth/auth.js
+++ src/store/auth/auth.js
@@ -1,27 +1,71 @@
+import axios from 'axios';
+
 /** @type {import('vuex').StoreOptions} */
 export const authStore = {
   state: () => ({
     user: null,
     error: null,
-    loading: false
+    loading: false,
+    userDetails: null,
+    fetchingUserDetails: false
   }),
 
   mutations: {
-    setUser(state, user) {
+    setLoggedUser(state, user) {
       state.error = null;
       state.loading = false;
       state.user = user;
+    },
+    fetchLoggedUserDetailsStart(state) {
+      state.fetchingUserDetails = true;
+      state.fetchUserDetailsError = null;
+    },
+    fetchLoggedUserDetailsSuccess(state, userData) {
+      state.userDetails = userData;
+      state.fetchingUserDetails = false;
+    },
+    fetchLoggedUserDetailsFail(state, errMsg) {
+      state.fetchUserDetailsError = errMsg;
     }
   },
 
   actions: {
-    authenticate({ commit }) {
+    async authenticate({ commit }) {
       const user = {
         id: '-zxf34gbb356-35f3',
         fname: 'Dean',
-        lname: 'Winchester'
+        lname: 'Winchester',
+        location: 'Rzeszów',
+        avatarUrl: 'http://localhost:3998/home/avatar-placeholder'
       };
-      commit('setUser', user);
+      commit('setLoggedUser', user);
+    },
+    async fetchLoggedUserDetails({ commit, state }) {
+      commit('fetchLoggedUserDetailsStart');
+      try {
+        const { data } = await axios.get(
+          'http://localhost:3998/home/user/' + state.user.id
+        );
+        const { userData } = data;
+        const userDetails = {
+          educations: userData.educations.map(x => ({
+            ...x,
+            start: new Date(x.start),
+            end: x.end === 'current' ? x.end : new Date(x.end)
+          })),
+          knowledge: [...userData.knowledge],
+          experience: userData.experience.map(x => ({
+            ...x,
+            start: new Date(x.start),
+            end: x.end === 'current' ? x.end : new Date(x.end)
+          })),
+          hobbies: [...userData.hobbies]
+        };
+
+        commit('fetchLoggedUserDetailsSuccess', userDetails);
+      } catch (err) {
+        commit('fetchLoggedUserDetailsFail', err.message);
+      }
     }
   }
 };
