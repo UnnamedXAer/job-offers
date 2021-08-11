96ae61a7a00b44781d3ffef0c1c298a9474837b7
UnnamedXAer <UnnamedXAer@gmail.com>
2021-08-11 10:47:06 +0200

    extract user info to separate component

--- src/components/user/MyAccount.vue
+++ src/components/user/MyAccount.vue
@@ -7,34 +7,7 @@
     >
     </app-edit-modal>
 
-    <section class="row mb-4">
-      <div class="col-12 col-sm-5 __avatar_container">
-        <div class="mx-auto __avatar_wrapper">
-          <img
-            class="img-thumbnail __avatar"
-            :src="user.avatarUrl ? user.avatarUrl : '@/assets/icons/user.svg'"
-            alt="Avatar image"
-          />
-        </div>
-      </div>
-
-      <div class="col-12 col-sm-7">
-        <table>
-          <tr>
-            <th>Name</th>
-            <td>{{ user.fname }} {{ user.lname }}</td>
-          </tr>
-          <tr>
-            <th>DOB</th>
-            <td>{{ user.DOB }}</td>
-          </tr>
-          <tr>
-            <th>Title</th>
-            <td>{{ user.title }}</td>
-          </tr>
-        </table>
-      </div>
-    </section>
+    <app-user-info></app-user-info>
 
     <app-error v-if="error">{{ error }}</app-error>
     <div
@@ -66,12 +39,14 @@ import EducationCardVue from './detailsCards/EducationCard.vue';
 import ExperienceCardVue from './detailsCards/ExperienceCard.vue';
 import KnowledgeCardVue from './detailsCards/KnowledgeCard.vue';
 import SimpleValueCardVue from './detailsCards/SimpleValueCard.vue';
+import UserInfoVue from './userInfo/UserInfo.vue';
 
 export default {
   name: 'MyProfile',
   components: {
     appEditModal: EditModalVue,
     appError: ErrorVue,
+    appUserInfo: UserInfoVue,
     appEducationCard: EducationCardVue,
     appExperienceCard: ExperienceCardVue,
     appKnowledgeCard: KnowledgeCardVue,
@@ -79,7 +54,6 @@ export default {
   },
   computed: {
     ...mapState({
-      user: (state) => state.auth.user,
       userDetails: (state) => state.auth.userDetails,
       editedField: (state) => state.auth.editUserDetails.editedField,
       loading: (state) => state.auth.fetchingUserDetails,
@@ -103,13 +77,5 @@ export default {
 </script>
 
 <style scoped>
-.__avatar_wrapper,
-.__avatar {
-  width: 200px;
-  height: 200px;
-}
 
-.__avatar {
-  border-radius: 50%;
-}
 </style>
---
+++ src/components/user/userInfo/UserInfo.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="row mb-4">
+    <div class="col-12 col-sm-5 __avatar_container">
+      <div class="mx-auto __avatar_wrapper">
+        <img
+          class="img-thumbnail __avatar"
+          :src="user.avatarUrl ? user.avatarUrl : '@/assets/icons/user.svg'"
+          alt="Avatar image"
+        />
+      </div>
+    </div>
+
+    <div class="col-12 col-sm-7">
+      <table>
+        <tr>
+          <th>Name</th>
+          <td>{{ user.fname }} {{ user.lname }}</td>
+        </tr>
+        <tr>
+          <th>DOB</th>
+          <td>{{ user.DOB || '-' }}</td>
+        </tr>
+        <tr>
+          <th>Title</th>
+          <td>{{ user.title || '-' }}</td>
+        </tr>
+      </table>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    user() {
+      return this.$store.state.auth.user;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.__avatar_wrapper,
+.__avatar {
+  width: 200px;
+  height: 200px;
+}
+
+.__avatar {
+  border-radius: 50%;
+}
+</style>
